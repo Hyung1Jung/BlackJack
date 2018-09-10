e13ece080ecf8296e5efea9976e2301e8defb7c4
HyungIlJung <junghyungile@gmail.com>
2018-09-10 19:38:00 +0900

    '19.09.10 19:37

--- src/blackJack/BlackJack.java
+++ src/blackJack/BlackJack.java
@@ -1,13 +1,36 @@
 package blackJack;
 
-public class BlackJack {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("201432031 정형일");
-		System.out.println("Hello World!");
-		System.out.println("Test");
-		System.out.println("Test1");
+class BlackJack extends JFrame {
+	
+	JButton b1;
+
+
+	public BlackJack() {
+		// 제목 설정
+		setTitle("BlackJack");
+		// X버튼 눌렀을 때 닫히도록 설정
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// 프레임 크기 지정
+		setSize(1000, 650);
+		// JPanel 객체 설정
+		JPanel panel = new JPanel();
+		// 배치관리자를 null로 설정
+		panel.setLayout(null);
+		// Jbutton 객체 설정
+		b1 = new JButton("Start");
+		// Panel 추가
+		panel.add(b1);
+		// 프레임 보이도록 설정
+		setVisible(true);
+		// 버튼 위치 설정
+		b1.setBounds(450, 480, 100, 60); // b.setBounds(x, y, w, h);
+		// 패널 추가
+		add(panel);
 	}
-
 }
+
+
